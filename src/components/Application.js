11ae8493c2fad3e8602f8a1d20cc68b0748455ce
Nick <maniutin@gmail.com>
2021-03-17 16:01:32 -0400

    add axios get req to Application

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,24 +5,6 @@ import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment";
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
-
 const appointments = [
   {
     id: 1,
@@ -71,6 +53,12 @@ const appointments = [
 ];
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+  useEffect(() => {
+    axios.get("/api/days").then((response) => {
+      setDays([...response.data]);
+    });
+  }, []);
   return (
     <main className="layout">
       <section className="sidebar">
--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,6 +2,7 @@ import React from "react";
 import DayListItem from "components/DayListItem";
 
 export default function DayList(props) {
+  console.log("DAYS:", props.days);
   return props.days.map((day) => (
     <DayListItem
       key={day.id}
