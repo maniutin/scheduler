d43e587783efe4c0779c5473a4de415c469f7c49
Nick <maniutin@gmail.com>
2021-03-18 17:33:57 -0400

    update Appointment component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,24 +1,38 @@
 import React from "react";
 import "components/Appointment/styles.scss";
 import Header from "components/Appointment/Header";
+import Form from "components/Appointment/Form";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 
+import useVisualMode from "hooks/useVisualMode";
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
+
 export default function Appointment(props) {
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
   return (
     <>
       <Header time={props.time} />{" "}
-      {props.interview ? (
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
         <Show
-          id={props.id}
-          time={props.time}
           student={props.interview.student}
           interviewer={props.interview.interviewer}
         />
-      ) : (
-        <div id={props.id}>
-          <Empty />
-        </div>
+      )}
+      {mode === CREATE && (
+        <Form
+          name={props.name}
+          interviewers={[]}
+          interviewer={3}
+          onSave={"onSave"}
+          onCancel={back}
+        />
       )}
     </>
   );
