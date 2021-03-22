b5b2fa652e6dc6c13511dcfb86a44b9357233356
Nick <maniutin@gmail.com>
2021-03-22 13:56:54 -0400

    add test to InterviewerList

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -2,6 +2,8 @@ import React from "react";
 import InterviewerListItem from "components/InterviewerListItem";
 import "components/InterviewerList.scss";
 
+import PropTypes from "prop-types";
+
 export default function InterviewerList(props) {
   const interviewers = props.interviewers.map((interviewer) => {
     return (
@@ -21,3 +23,7 @@ export default function InterviewerList(props) {
     </section>
   );
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired,
+};
